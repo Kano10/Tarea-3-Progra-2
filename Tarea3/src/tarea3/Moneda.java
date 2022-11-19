bb3067cfd0f2cf387d59770a43f307f95adf52eb
Lirayen Martinez <lirmartinez2021@udec.cl>
2022-11-18 23:06:16 -0300

    Avance Monedas

--- Tarea3/src/tarea3/Moneda.java
+++ Tarea3/src/tarea3/Moneda.java
@@ -1,16 +1,33 @@
 package tarea3;
 
-public abstract class Moneda {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public abstract class Moneda extends JLabel {
+
+    public Moneda() {
+        super();
+    }
 
     public Moneda getSerie() {
         return this;
+    }
+
+    public void setXY(int x1, int y1) {
+        this.setLocation(x1, y1);
     }
 
     public abstract int getValor();
 }
 
 class M500 extends Moneda {
+ public M500() {
+        super();
 
+        ImageIcon imagen = new ImageIcon(getClass().getResource("/Tarea-3-progra-2/500.png"));
+        this.setIcon(imagen);
+        this.setSize(imagen.getIconWidth(),imagen.getIconHeight());
+    }
     @Override
     public int getValor() {
         return 500;
@@ -18,7 +35,13 @@ class M500 extends Moneda {
 }
 
 class M100 extends Moneda {
+ public M100() {
+        super();
 
+        ImageIcon imagen = new ImageIcon(getClass().getResource("/Tarea-3-progra-2/100.png"));
+        this.setIcon(imagen);
+        this.setSize(imagen.getIconWidth(),imagen.getIconHeight());
+    }
     @Override
     public int getValor() {
         return 100;
@@ -26,7 +49,13 @@ class M100 extends Moneda {
 }
 
 class M1000 extends Moneda {
+ public M1000() {
+        super();
 
+        ImageIcon imagen = new ImageIcon(getClass().getResource("/Tarea-3-progra-2/1000.png"));
+        this.setIcon(imagen);
+        this.setSize(imagen.getIconWidth(),imagen.getIconHeight());
+    }
     @Override
     public int getValor() {
         return 1000;
